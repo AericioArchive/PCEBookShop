10e6fd9c063447a2a1ee81e22af1a80bba77b574
Coot302 <93241079+Coot302@users.noreply.github.com>
2022-06-07 17:56:55 +0200

    Update PCEBookShop.php

--- src/Aericio/PCEBookShop/PCEBookShop.php
+++ src/Aericio/PCEBookShop/PCEBookShop.php
@@ -4,57 +4,48 @@ declare(strict_types=1);
 
 namespace Aericio\PCEBookShop;
 
-use Aericio\PCEBookShop\commands\BookShopCommand;
-use Aericio\PCEBookShop\tasks\CheckUpdatesTask;
-use Aericio\PCEBookShop\utils\Utils;
-use CortexPE\Commando\exception\HookAlreadyRegistered;
-use CortexPE\Commando\PacketHooker;
-use DaPigGuy\libPiggyEconomy\exceptions\MissingProviderDependencyException;
-use DaPigGuy\libPiggyEconomy\exceptions\UnknownProviderException;
-use DaPigGuy\libPiggyEconomy\libPiggyEconomy;
-use DaPigGuy\libPiggyEconomy\providers\EconomyProvider;
-use DaPigGuy\PiggyCustomEnchants\CustomEnchantManager;
-use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 use pocketmine\utils\Config;
+use pocketmine\scheduler\Task;
+use pocketmine\lang\Translatable;
+use pocketmine\plugin\PluginBase;
+use CortexPE\Commando\PacketHooker;
+use Aericio\PCEBookShop\utils\Utils;
+use Aericio\PCEBookShop\commands\BookShopCommand;
+use DaPigGuy\PiggyCustomEnchants\CustomEnchantManager;
 
 class PCEBookShop extends PluginBase
 {
-    /** @var Config */
-    private $messages;
+    private static PCEBookShop $instance;
 
-    /** @var EconomyProvider */
-    public $economyProvider;
+    private Config $messages;
 
-    /** @var array */
-    public $enchantments = [];
+    public array $enchantments = [];
 
-    /**
-     * @throws HookAlreadyRegistered
-     * @throws MissingProviderDependencyException
-     * @throws UnknownProviderException
-     */
     public function onEnable(): void
     {
+        self::$instance = $this;
         $this->saveResource("messages.yml");
         $this->messages = new Config($this->getDataFolder() . "messages.yml");
         $this->saveDefaultConfig();
-
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 
-        libPiggyEconomy::init();
-        $this->economyProvider = libPiggyEconomy::getProvider($this->getConfig()->get("economy"));
-
         if (!PacketHooker::isRegistered()) PacketHooker::register($this);
-        $this->getServer()->getCommandMap()->register("pcebookshop", new BookShopCommand($this, "pcebookshop", "Opens the PiggyCustomEnchants Book Shop Menu", ["bookshop", "bs"]));
+        $this->getServer()->getCommandMap()->register("ceshop", new BookShopCommand($this, "cebuy", "Custom Enchantments Shop", ["ceshop", "cebuy"]));
 
-        $this->getServer()->getAsyncPool()->submitTask(new CheckUpdatesTask());
-
-        foreach (CustomEnchantManager::getEnchantments() as $enchants) {
-            $excluded = $this->getConfig()->get("excluded-enchants", []);
-            if (!in_array($enchants->getId(), $excluded) && !in_array(strtolower($enchants->getName()), $excluded)) {
-                $this->enchantments[$enchants->getRarity()][] = $enchants;
+        $this->getScheduler()->scheduleDelayedTask(new class extends Task
+        {
+            public function onRun(): void
+            {
+                foreach (CustomEnchantManager::getEnchantments() as $enchants) {
+                    $excluded = PCEBookShop::getInstance()->getConfig()->get("excluded-enchants", []);
+                    $enchantName = $enchants->getName() instanceof Translatable ? Server::getInstance()->getLanguage()->translate($enchants->getName()) : Server::getInstance()->getLanguage()->translateString($enchants->getName());
+                    if (!in_array($enchants->getId(), $excluded) && !in_array(strtolower($enchantName), $excluded)) {
+                        PCEBookShop::getInstance()->enchantments[$enchants->getRarity()][] = $enchants;
+                    }
+                }
             }
-        }
+        }, 100);
     }
 
     public function getMessage(string $key, array $tags = []): string
@@ -62,13 +53,11 @@ class PCEBookShop extends PluginBase
         return Utils::translateColorTags(str_replace(array_keys($tags), $tags, $this->messages->getNested($key, $key)));
     }
 
-    public function getEconomyProvider(): EconomyProvider
-    {
-        return $this->economyProvider;
-    }
-
     public function getEnchantmentsByRarity(int $rarity): array
     {
+        if (!isset($this->enchantments[$rarity]) || count($this->enchantments[$rarity]) === 0) {
+            return [];
+        }
         return $this->enchantments[$rarity];
     }
 
@@ -78,5 +67,10 @@ class PCEBookShop extends PluginBase
     public function getRandomWeightedElement(int $max): int
     {
         return intval(floor(1 + pow(lcg_value(), $this->getConfig()->getNested('chance.gamma', 1.5)) * $max));
+    }
+
+    public static function getInstance(): PCEBookShop
+    {
+        return self::$instance;
     }
 }
